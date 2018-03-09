cf29288feb39f37a392f303920dfd50ec7c260b4
Michael Coblenz <mcoblenz@gmail.com>
2018-03-09 15:27:54 -0500

    Fixed shell script.

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -d "$HOME/protobuf/" ]; then
     wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
     unzip protoc-3.5.1-linux-x86_64.zip
-    mv bin/protoc $/HOME/protobuf/
+    mv bin/protoc ${HOME}/protobuf/
 else
     echo "Using cached directory."
     fi
